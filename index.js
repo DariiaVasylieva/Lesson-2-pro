6db7eb7a06101392670f8b1f4dedf45b1f1c3b2c
DariiaVasylieva <vasil.dasha04@gmail.com>
2023-04-25 13:17:41 +0200

    finished work

---
+++ index.js
@@ -0,0 +1,13 @@
+firstString = prompt('Введите строку 1');
+secondString = prompt('Введите строку 2');
+thirdString = prompt('Введите строку 3');
+
+alert(`${firstString} ${secondString} ${thirdString}`);
+
+fiveDigitNumber = prompt('Введите пятизначное число:');
+firstNumber = fiveDigitNumber[0];
+secondNumber = fiveDigitNumber[1];
+thirdNumber = fiveDigitNumber[2];
+fourthNumber = fiveDigitNumber[3];
+fifthNumber = fiveDigitNumber[4];
+alert(`${firstNumber} ${secondNumber} ${thirdNumber} ${fourthNumber} ${fifthNumber}`);
